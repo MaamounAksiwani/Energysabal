60940be2bd185bde0b7c1e3aba0bb6c920fde4f6
Maamoun <maamoun.malkiswani@gmail.com>
2024-03-02 22:28:49 +0300

    fix

--- src/components/ReportScreen/SectionTexr/SectionText.jsx
+++ src/components/ReportScreen/SectionTexr/SectionText.jsx
@@ -6,7 +6,7 @@ const SectionText = () => {
     return (
         <div style={{ backgroundColor: '#80AA45', padding: '50px 0 ', textAlign: 'center' }} >
             <Container maxWidth='lg'>
-                <h1 style={{color:'#10271D', fontSize:'25px', fontWeight:'500' , padding:'25px'}}>Ready to take the first step towards a greener, more sustainable
+                <h1 style={{color:'#fff', fontSize:'25px', fontWeight:'500' , padding:'25px'}}>Ready to take the first step towards a greener, more sustainable
                     future? Choose the option that best suits your needs and fill out our
                     online form to get started with your free Energy Sabal electrification
                     report today. Our team will generate your report promptly</h1>
